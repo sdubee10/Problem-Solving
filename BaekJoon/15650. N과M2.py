e5ba08ddf097d4013e41a2737583ef688e93fec9
Seungwan <sdubee10@gmail.com>
2021-11-23 17:40:21 +0900

    N과M 2

---
+++ BaekJoon/15650. N과M2.py
@@ -0,0 +1,17 @@
+n, m = map(int ,input().split())
+
+def DFS(L, s):
+    if L == m:
+        print(" ".join(map(str, res)))
+    else:
+        for i in range(s, n+1):
+            if ch[i] == 0:
+                ch[i] = 1
+                res[L] = i
+                DFS(L + 1, i)
+                ch[i] = 0
+
+ch = [0] * (n+1)
+res = [0] * (m)
+
+DFS(0, 1)
