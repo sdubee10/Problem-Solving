cbf3e331f0f45fcb6c14a0f497d2e744903b2e30
Seungwan <sdubee10@gmail.com>
2021-11-24 15:40:45 +0900

    BFS Snake and Ladder Retry

--- BaekJoon/16928. 뱀과 사다리 게임.py
+++ BaekJoon/16928. 뱀과 사다리 게임.py
@@ -1,16 +1,18 @@
 from collections import deque
+
 n, m = map(int, input().split())
 
 board = [i for i in range(101)]
-visited = [-1] * 101
+dist = [-1] * 101
 
 for _ in range(n+m):
-    x, y = map(int, input().split())
+    x, y = map(int,input().split())
     board[x] = y
 
 queue = deque()
-visited[1] = 0
+dist[1] = 0
 queue.append(1)
+
 while queue:
     now = queue.popleft()
     for i in range(1, 7):
@@ -18,8 +20,7 @@ while queue:
         if y > 100:
             continue
         y = board[y]
-        if visited[y] == -1 or visited[y] > visited[now]+1:
-            visited[y] = visited[now] + 1
+        if dist[y] == -1 or dist[y] > dist[now] + 1:
+            dist[y] = dist[now] + 1
             queue.append(y)
-
-print(visited[-1])
+print(dist[-1])
