a10923e9754c42e22693a156034d96b4e8a32ebc
Seungwan <sdubee10@gmail.com>
2021-12-09 20:50:40 +0900

    Spanning Tree

---
+++ BaekJoon/1922. 네트워크 연결.py
@@ -0,0 +1,63 @@
+# import sys
+# input=sys.stdin.readline
+#
+# def find(x):
+#    if x==parent[x]:
+#       return x
+#    parent[x]=find(parent[x])
+#    return parent[x]
+#
+# def union(x,y):
+#    x,y=find(x),find(y)
+#    parent[x]=y
+#
+# n=int(input())
+# m=int(input())
+# arr=[list(map(int,input().split())) for _ in range(m)]
+# arr=sorted(arr,key=lambda k: k[2])
+# parent =[i for i in range(0,n+2)]
+#
+# ans=0
+# for a in arr:
+#    start,end,weight=a
+#    #cycle 확인
+#    if find(start)==find(end):
+#       continue
+#    else:
+#       ans+=weight
+#       union(start,end)
+# print(ans)
+
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+arr =[list(map(int, input().split())) for i in range(m)]
+
+arr = sorted(arr, key=lambda x:x[2])
+
+parent = [i for i in range(n+2)]
+
+def find(x):
+   if x == parent[x]:
+      return x
+   parent[x] = find(parent[x])
+   return parent[x]
+
+def union(x, y):
+   fx = find(x)
+   fy = find(y)
+   if fx != fy:
+      parent[fx] = fy
+
+ans = 0
+for a in arr:
+   start, end, weight = a
+   if find(start) == find(end):
+      continue
+   else:
+      ans += weight
+      union(start, end)
+print(ans)
