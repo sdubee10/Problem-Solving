422af4a99704fe6cb9bfa3bb9de521556d9d1780
Seungwan <sdubee10@gmail.com>
2021-12-22 01:47:48 +0900

    BFS retry

--- BaekJoon/16928. 뱀과 사다리 게임.py
+++ BaekJoon/16928. 뱀과 사다리 게임.py
@@ -1,26 +1,28 @@
+import sys
 from collections import deque
 
+input = sys.stdin.readline
 n, m = map(int, input().split())
 
-board = [i for i in range(101)]
-dist = [-1] * 101
+nxt = [i for i in range(101)]
+check = [-1] * (101)
 
-for _ in range(n+m):
-    x, y = map(int,input().split())
-    board[x] = y
+for i in range(n + m):
+    x, y = map(int, input().split())
+    nxt[x] = y
 
 queue = deque()
-dist[1] = 0
+check[1] = 0
 queue.append(1)
 
 while queue:
     now = queue.popleft()
+
     for i in range(1, 7):
-        y = now + i
-        if y > 100:
-            continue
-        y = board[y]
-        if dist[y] == -1 or dist[y] > dist[now] + 1:
-            dist[y] = dist[now] + 1
-            queue.append(y)
-print(dist[-1])
+        move = now + i
+        if move <= 100:
+            move = nxt[move]
+            if check[move] == -1 or check[move] > check[now] + 1:
+                check[move] = check[now] + 1
+                queue.append(move)
+print(check[-1])
