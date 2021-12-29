f9a435413120414cdedca40ccf268dc0b952a974
Seungwan <sdubee10@gmail.com>
2021-12-29 21:36:52 +0900

    use zip function

---
+++ 프로그래머스/Level2/Lv2. 전화번호 목록.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    phone_book.sort()
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        # print(p1, p2)
+        if p2.startswith(p1):
+            return False
+    return True
+
+print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123","456","789"]))
+print(solution(["12","123","1235","567","88"]))
