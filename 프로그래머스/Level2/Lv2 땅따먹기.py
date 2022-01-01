59cc92577702bb3c82e821e13ead7cb4f6fbc6f7
Seungwan <sdubee10@gmail.com>
2022-01-01 23:29:16 +0900

    list

---
+++ 프로그래머스/Level2/Lv2 땅따먹기.py
@@ -0,0 +1,11 @@
+def solution(land):
+    n = len(land)
+
+    for i in range(1,n):
+        for j in range(4):
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+
+    answer = max(land[n-1])
+    return answer
+
+print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
