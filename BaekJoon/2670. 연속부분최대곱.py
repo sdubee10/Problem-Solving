42a276438979d55a5a6bc56e302df990520e356e
Seungwan <sdubee10@gmail.com>
2022-01-09 00:01:44 +0900

    round up using %.f format

--- BaekJoon/2670. 연속부분최대곱.py
+++ BaekJoon/2670. 연속부분최대곱.py
@@ -1,20 +1,14 @@
-# import sys
-#
-# input = sys.stdin.readline
-#
-# n = int(input())
-#
-# nums = []
-# for i in range(n):
-#     nums.append(float(input()))
-#
-# for i in range(1, n):
-#     nums[i] = max(nums[i], nums[1-i]*nums[i])
-#
-# print(round(max(nums), 3))
+import sys
 
-N = int(input())
-nums = [float(input()) for _ in range(N)]
-for i in range(1, N):
-    nums[i] = max(nums[i - 1] * nums[i], nums[i])
-print("{:.3f}".format(max(nums)))
+input = sys.stdin.readline
+
+n = int(input())
+
+nums = []
+for i in range(n):
+    nums.append(float(input()))
+
+for i in range(1, n):
+    nums[i] = max(nums[i], nums[i-1]*nums[i])
+
+print("%.3f" % (max(nums)))
