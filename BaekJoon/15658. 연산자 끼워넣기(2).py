3bbb5404860bbf1b2c9ef09579988b8d1d46a7fe
Seungwan <sdubee10@gmail.com>
2022-01-13 14:27:19 +0900

    DFS math

---
+++ BaekJoon/15658. 연산자 끼워넣기(2).py
@@ -0,0 +1,52 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
+
+minnum = 1e9
+maxnum = -1e9
+
+def DFS(cnt, res, add, sub, mul, div):
+    global minnum, maxnum
+
+    if cnt == n:
+        minnum = min(minnum, res)
+        maxnum = max(maxnum, res)
+        return
+
+    else:
+        if add:
+            DFS(cnt + 1, res + nums[cnt], add - 1, sub, mul, div)
+        if sub:
+            DFS(cnt + 1, res - nums[cnt], add, sub - 1, mul, div)
+        if mul:
+            DFS(cnt + 1, res * nums[cnt], add, sub, mul - 1, div)
+        if div:
+            DFS(cnt + 1, int(res / nums[cnt]), add, sub, mul, div - 1)
+
+DFS(1, nums[0], add, sub, mul, div)
+print(maxnum)
+print(minnum)
+
+# def DFS(cnt, res, add, sub, mul, div):
+#     global minnum, maxnum
+#     if cnt == n:
+#         minnum = min(minnum, res)
+#         maxnum = max(maxnum, res)
+#         return
+#     else:
+#         if add:
+#             DFS(cnt+1, res+nums[cnt], add-1, sub, mul, div)
+#         if sub:
+#             DFS(cnt+1, res-nums[cnt], add, sub-1, mul, div)
+#         if mul:
+#             DFS(cnt+1, res*nums[cnt], add, sub, mul-1, div)
+#         if div:
+#             DFS(cnt+1, int(res/nums[cnt]), add, sub, mul, div-1)
+#
+# DFS(1, nums[0], add, sub, mul, div)
+# print(maxnum)
+# print(minnum)
