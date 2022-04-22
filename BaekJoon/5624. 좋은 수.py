a2f28876ff0b73249afc2299a57174554f00e0b1
Seungwan <sdubee10@gmail.com>
2022-04-23 01:27:44 +0900

    reduce N complexity by a+b = d-c

---
+++ BaekJoon/5624. 좋은 수.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+s = set()
+
+arr = list(map(int, input().split()))
+
+result = 0
+s.add(arr[0] + arr[0])
+
+for i in range(1, n):
+    for j in range(i):
+        if arr[i] - arr[j] in s:
+            result += 1
+            break
+
+    for j in range(i + 1):
+        s.add(arr[i] + arr[j])
+
+print(result)
